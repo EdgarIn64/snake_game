ee2233348c505d3f811f44b0e6bd2523079adb26
EdgarIn64 <edgar_coriaSixtyFour@hotmail.com>
2023-09-05 21:39:44 -0600

    snake head

--- snake.py
+++ snake.py
@@ -1,4 +1,7 @@
 import turtle
+import time
+
+wait = 0.1
 
 # Settings for the Window
 wn = turtle.Screen()
@@ -6,4 +9,35 @@ wn.title("Snake Python")
 wn.bgcolor("black")
 wn.setup(width = 600, height = 600)
 wn.tracer(0)
+
+
+# Snake Head
+cabeza = turtle.Turtle()
+cabeza.speed(0)
+cabeza.shape("square")
+cabeza.color("white")
+cabeza.penup()
+cabeza.goto(0,0)
+cabeza.direction = "left"
+
+# Functions
+
+def mov():
+	if cabeza.direction == "up" :
+		y = cabeza.ycor()
+		cabeza.sety(y + 20)
+	elif cabeza.direction == "right" :
+		x = cabeza.xcor()
+		cabeza.setx(x + 20)
+	elif cabeza.direction == "down" :
+		y = cabeza.ycor()
+		cabeza.sety(y - 20)
+	elif cabeza.direction == "left" :
+		x = cabeza.xcor()
+		cabeza.setx(x - 20)
+
+while True :
+	wn.update()
+	mov()
+	time.sleep(wait)
 
