8c37e3c8825ac51c91bbc1ef902252b5f40d8a73
EdgarIn64 <edgar_coriaSixtyFour@hotmail.com>
2023-09-06 20:56:17 -0600

    food

--- snake.py
+++ snake.py
@@ -1,5 +1,6 @@
 import turtle
 import time
+import random
 
 wait = 0.1
 
@@ -12,32 +13,61 @@ wn.tracer(0)
 
 
 # Snake Head
-cabeza = turtle.Turtle()
-cabeza.speed(0)
-cabeza.shape("square")
-cabeza.color("white")
-cabeza.penup()
-cabeza.goto(0,0)
-cabeza.direction = "left"
+snakeHead = turtle.Turtle()
+snakeHead.speed(0)
+snakeHead.shape("square")
+snakeHead.color("white")
+snakeHead.penup()
+snakeHead.goto(0,0)
+snakeHead.direction = "left"
+
+# Food
+food = turtle.Turtle()
+snakeHead.shape("circle")
+snakeHead.color("red")
+snakeHead.penup()
+snakeHead.goto(0,100)
+
 
 # Functions
+def up():
+	snakeHead.direction = "up"
+def right():
+	snakeHead.direction = "right"
+def down():
+	snakeHead.direction = "down"
+def left():
+	snakeHead.direction = "left"
 
 def mov():
-	if cabeza.direction == "up" :
-		y = cabeza.ycor()
-		cabeza.sety(y + 20)
-	elif cabeza.direction == "right" :
-		x = cabeza.xcor()
-		cabeza.setx(x + 20)
-	elif cabeza.direction == "down" :
-		y = cabeza.ycor()
-		cabeza.sety(y - 20)
-	elif cabeza.direction == "left" :
-		x = cabeza.xcor()
-		cabeza.setx(x - 20)
+	if snakeHead.direction == "up" :
+		y = snakeHead.ycor()
+		snakeHead.sety(y + 20)
+	elif snakeHead.direction == "right" :
+		x = snakeHead.xcor()
+		snakeHead.setx(x + 20)
+	elif snakeHead.direction == "down" :
+		y = snakeHead.ycor()
+		snakeHead.sety(y - 20)
+	elif snakeHead.direction == "left" :
+		x = snakeHead.xcor()
+		snakeHead.setx(x - 20)
+
+# Keyboard
+wn.listen()
+wn.onkeypress(up, "Up")
+wn.onkeypress(right, "Right")
+wn.onkeypress(down, "Down")
+wn.onkeypress(left, "Left")
 
 while True :
 	wn.update()
+
+	if snakeHead.distance(food) < 20 :
+		x = random.randint(-280, 280)
+		y = random.randint(-280, 280)
+		comida.goto(x, y)
+
 	mov()
 	time.sleep(wait)
 
